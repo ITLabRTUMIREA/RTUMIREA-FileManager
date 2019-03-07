009382dba018dd5229e54b2f1db45676f7431365
Uzhakov Valentine <yuz-valya@yandex.ru>
2019-03-07 18:04:33 +0300

    add dbContext

---
+++ DocumentManager/DataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManager
+{
+    public class DataBaseContext : IdentityDbContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
