0bb956be2c8af0d881db26d16f7af96948a4b663
Uzhakov Valentine <yuz-valya@yandex.ru>
2019-03-07 18:41:02 +0300

    add dbContext

---
+++ DocumentManager/DataBaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentManager
+{
+    public class DataBaseContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        DbSet<User> Clients { get; set; }
+
+
+    }
+}
