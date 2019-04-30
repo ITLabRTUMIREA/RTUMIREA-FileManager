dcbf3a567e7ec70b00d22ac0641b3d127402630c
ReC <kitanin.ser@mail.ru>
2019-04-30 12:56:59 +0300

    Began LoginPage

---
+++ DocumentManager/Pages/Login/LoginViewModel.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DocumentManager.Pages.Login
+{
+    public class LoginViewModel : PageModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public void OnGet()
+        {
+
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -7,6 +7,12 @@ namespace Models
 {
     public class User : IdentityUser<Guid>
     {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
 
     }
 }
