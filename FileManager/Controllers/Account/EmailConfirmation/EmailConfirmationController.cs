203b8e8e79b8de3fd6bc276853fd79e61cc329ff
ReC <kitanin.ser@mail.ru>
2019-06-29 23:30:14 +0300

    Made controller for email confirmation

---
+++ FileManager/Controllers/Account/EmailConfirmation/EmailConfirmationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FileManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileManager.Controllers.Account.EmailConfirmation
+{
+    [Route("Account/[controller]")]
+    [ApiController]
+    public class EmailConfirmationController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public EmailConfirmationController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            User user = await _userManager.FindByIdAsync(userId);
+            IdentityResult emailConfirmationResult = await _userManager.ConfirmEmailAsync(user, token);
+
+            if (emailConfirmationResult.Succeeded)
+            {
+                return Content("Email Verified");
+            }
+
+            return Content("Invalid Email Verification Token");
+
+        }
+    }
+}
