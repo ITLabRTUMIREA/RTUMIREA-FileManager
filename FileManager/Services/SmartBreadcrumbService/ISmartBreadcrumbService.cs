01776d8d006db70fae0d425500e6f42a19b4de00
ReC <kitanin.ser@mail.ru>
2019-08-30 20:43:29 +0300

    Made breadcrumb service for getting breadcrumbs. Added Department Page

---
+++ FileManager/Services/SmartBreadcrumbService/ISmartBreadcrumbService.cs
@@ -0,0 +1,14 @@
+﻿using SmartBreadcrumbs.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileManager.Services.SmartBreadcrumbService
+{
+    public interface ISmartBreadcrumbService
+    {
+        RazorPageBreadcrumbNode GetReportingYearBreadCrumbNode(Guid yearId);
+        RazorPageBreadcrumbNode GetDepartmentBreadCrumbNode(Guid departmentId, RazorPageBreadcrumbNode parentBreadcrumbNode);
+    }
+}
